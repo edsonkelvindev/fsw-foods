98b6ffa12f3e1b944027a6cbd6de0d1976853141
Edson Kelvin <edsonkelvin19@gmail.com>
2024-05-05 19:49:59 -0300

    fix: add modify migration

--- prisma/migrations/20240501054225_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `discountPercenage` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "discountPercenage",
-ADD COLUMN     "discountPercentage" INTEGER NOT NULL DEFAULT 0;
